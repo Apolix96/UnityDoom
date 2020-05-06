660ab30c2b39f6da2d56338b8766e23c53b248ee
Andrey Evtukhov <48060782+Apolix96@users.noreply.github.com>
2020-05-06 19:46:52 +0500

    +add New changes

---
+++ DemoDoom/Assets/Player/FPSInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Scripts/FPS Input")]
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+
+    private CharacterController _charController;
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        float deltax = Input.GetAxis("Horizontal") * speed;
+        float deltaz = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltax, 0, deltaz);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    
+    }
+
+}
